8341a0e2c00472328826b40bf9690911307d93c3
Marcos Iglesias <golodhros@gmail.com>
2012-09-23 00:17:04 -0700

    Improved Tests

--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@ $('#testing-page').live('pageinit', function(event) {
 	$(document).on('pageshow', function(){
 		var $carouselWrapperElement = $('.carousel-wrapper'),
 			$carouselElement 		= $('.carousel');
+
+		module("Basic Carousel most useful public methods");
 		
 		test( "Get Current Slide Index Test", function() {
 			equal( $('.carousel-wrapper').carousel('getCurrentSlideIndex'), 0);	
@@ -18,13 +20,21 @@ $('#testing-page').live('pageinit', function(event) {
 			equal( $('.carousel-wrapper').carousel('getCurrentSlideIndex'), 0);	
 		});
 		
-		test( "Two Swipes Test", function() {
+		test( "Reset Carousel Test", function() {
 			$carouselWrapperElement.carousel('swipeLeft');
 			$carouselWrapperElement.carousel('swipeLeft');
 			$carouselWrapperElement.carousel('swipeRight');
-			$carouselWrapperElement.carousel('swipeRight');
+			$carouselWrapperElement.carousel('resetCarousel');
 			equal( $('.carousel-wrapper').carousel('getCurrentSlideIndex'), 0);	
 		});
+		
+		test( "Orientation Change Test", function() {
+			$carouselWrapperElement.carousel('swipeLeft');
+			$('.ui-page').trigger('orientationchange');			
+			equal( $('.carousel-wrapper').carousel('getCurrentSlideIndex'), 0);	
+		});
+		
+		module("Basic Carousel internal methods");
 		
 		test( "Counters Test", function() {
 			$carouselWrapperElement.carousel('setDotCounter', 0);
@@ -46,17 +56,31 @@ $('#testing-page').live('pageinit', function(event) {
 			equal( $('.position em').find('on').length, 0);	
 		});
 		
-		test( "Orientation Change Test", function() {
-			$carouselWrapperElement.carousel('swipeLeft');
-			$('.ui-page').trigger('orientationchange');			
-			equal( $('.carousel-wrapper').carousel('getCurrentSlideIndex'), 0);	
-		});
-		
-		test( "Reset Carousel Test", function() {
+		module("Basic Carousel public methods stress test");
+
+		test( "Two Swipes Test", function() {
 			$carouselWrapperElement.carousel('swipeLeft');
 			$carouselWrapperElement.carousel('swipeLeft');
 			$carouselWrapperElement.carousel('swipeRight');
-			$carouselWrapperElement.carousel('resetCarousel');
+			$carouselWrapperElement.carousel('swipeRight');
+			equal( $('.carousel-wrapper').carousel('getCurrentSlideIndex'), 0);	
+		});
+		
+		test( "Zig-Zag Swipes Test", function() {
+			$carouselWrapperElement.carousel('swipeLeft');
+			$carouselWrapperElement.carousel('swipeRight');
+			$carouselWrapperElement.carousel('swipeLeft');
+			$carouselWrapperElement.carousel('swipeRight');
+			equal( $('.carousel-wrapper').carousel('getCurrentSlideIndex'), 0);	
+		});
+
+		test( "Three Swipes Test", function() {
+			$carouselWrapperElement.carousel('swipeLeft');
+			$carouselWrapperElement.carousel('swipeLeft');
+			$carouselWrapperElement.carousel('swipeLeft');
+			$carouselWrapperElement.carousel('swipeRight');
+			$carouselWrapperElement.carousel('swipeRight');
+			$carouselWrapperElement.carousel('swipeRight');
 			equal( $('.carousel-wrapper').carousel('getCurrentSlideIndex'), 0);	
 		});
 	});
